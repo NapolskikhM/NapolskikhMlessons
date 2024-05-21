4243a88c00c102a0f64818242cda338e0b8adc85
NapolskikhM <m9217203819@rambler.ru>
2024-05-21 11:26:21 +0300

    Delete homework2.py

--- homework2.py
+++
@@ -1,8 +0,0 @@
-name = 'Михаил'
-print('name: ', name, sep = '')
-age = 56
-print('age: ', age, sep = '')
-new_age = age + 1
-print('new age: ', new_age, sep = '')
-is_student = True
-print('is_student: ', is_student, sep = '')
