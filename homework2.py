7679747343ea458587c153f14a32a9a455ab55ce
NapolskikhM <m9217203819@rambler.ru>
2024-05-21 11:03:50 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+name = 'Михаил'
+print('name: ', name, sep = '')
+age = 56
+print('age: ', age, sep = '')
+new_age = age + 1
+print('new age: ', new_age, sep = '')
+is_student = True
+print('is_student: ', is_student, sep = '')
