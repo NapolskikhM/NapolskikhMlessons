b851e330a2cc2467f354049e5f6e6b4f474e48c2
NapolskikhM <m9217203819@rambler.ru>
2024-05-21 11:26:50 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework_amt = 12
+hours_amt = 1.5
+course_name = "Python"
+one_homework_time = hours_amt / homework_amt
+print("Курс: ", course_name, ", всего задач: ", homework_amt, ", затрачено часов:", hours_amt, ", среднее время выполнения ", one_homework_time, sep="")
---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Михаил'
+print('Name: ', name, sep = '')
+age = 56
+print('Age: ', age, sep = '')
+new_age = age + 1
+print('New Age: ', new_age, sep = '')
+is_student = True
+print('Is Student: ', is_student, sep = '')
