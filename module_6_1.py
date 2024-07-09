3e17c91a4dbd334ac7293211f40eb076c8e5ca76
NapolskikhM <m9217203819@rambler.ru>
2024-07-09 08:31:02 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,85 @@
+class Animal:
+
+    def __init__(self, name, alive=True, fed=False):
+        self.name = name
+        self.alive = alive
+        self.fed = fed
+
+
+class Plant:
+
+    def __init__(self, name, edible):
+        self.name = name
+        self.edible = False
+
+
+class Mammal(Animal):
+
+    def __init__(self, name, alive=True, fed=False):
+        super().__init__(name, alive, fed)
+
+    def eat(self, food):
+        if food.edible:
+            print(self.name, ' съел ', food.name)
+            self.fed = True
+        else:
+            print(self.name, ' не стал есть ', food.name)
+            self.alive = False
+
+
+class Predator(Animal):
+
+    def __init__(self, name, alive=True, fed=False):
+        super().__init__(name, alive, fed)
+
+    def eat(self, food):
+        if food.edible:
+            print(self.name, ' съел ', food.name)
+            self.fed = True
+        else:
+            print(self.name, ' не стал есть ', food.name)
+            self.alive = False
+
+
+class Flower(Plant):
+
+    def __init__(self, name, edible=False):
+        super().__init__(name, edible)
+        self.name = name
+        self.edible = edible
+
+
+class Fruit(Plant):
+
+    def __init__(self, name, edible=True):
+        super().__init__(name, edible)
+        self.name = name
+        self.edible = edible
+
+
+# Проверочный код
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
+# вывод на консоль
+
+'''Волк с Уолл-Стрит
+Цветик семицветик
+True
+False
+Волк с Уолл-Стрит не стал есть Цветик семицветик
+Хатико съел Заводной апельсин
+False
+True'''
