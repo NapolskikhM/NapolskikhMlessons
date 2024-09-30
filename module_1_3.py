f4907068239bd31e3d91a4ca00cc2d5e78d7902b
NapolskikhM <m9217203819@rambler.ru>
2024-09-30 10:56:24 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Михаил'
+print('Name: ', name, sep='')
+age = 56
+print('Age: ', age, sep='')
+age += 1
+print('New Age: ', age, sep='')
+is_student = True
+print('Is Student: ', is_student, sep='')
