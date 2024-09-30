e5e50cbbf96cf1d9f17f42b17561e46b3c1936f0
NapolskikhM <m9217203819@rambler.ru>
2024-09-30 17:16:32 +0300

    Update module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -7,16 +7,20 @@ username TEXT NOT NULL, email TEXT NOT NULL, age INTEGER, balance INTEGER)''')
 
 cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
 
+#создаем записи
 for i in range(1, 11):
     cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
                    (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', '1000'))
 
+#меняем balance у каждой второй записи
 for i in range(1, 11, 2):
     cursor.execute('UPDATE Users SET balance = ? WHERE id = ?', ('500', f'{i}'))
 
+#удаляем каждую третью
 for i in range(1, 11, 3):
     cursor.execute('DELETE FROM Users WHERE id = ?', (f'{i}',))
 
+#выводим записи кроме age = 60
 cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
 users = cursor.fetchall()
 for user in users:
