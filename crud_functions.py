8444f6d8ba6edd34e1f1263b98a787e6defd0b96
NapolskikhM <m9217203819@rambler.ru>
2024-10-04 11:27:17 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -1,10 +1,11 @@
 # aiogram 2.25.1 python 3.10
+
 import sqlite3
 
 
 def initiate_db():
 
-    # создаем таблицу
+    # создаем таблицу продукты
     connection = sqlite3.connect('products.db')
     cursor = connection.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS Products(id INTEGER PRIMARY KEY, 
@@ -19,6 +20,43 @@ def initiate_db():
 
     connection.commit()
     connection.close()
+
+    # создаем таблицу пользователи
+    connection = sqlite3.connect('users.db')
+    cursor = connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY, 
+        username TEXT NOT NULL, email TEXT NOT NULL, age INTEGER NOT NULL, balance INTEGER NOT NULL)''')
+    cursor.execute("CREATE INDEX IF NOT EXISTS index_Users ON Users(id)")
+
+    # заполняем
+    for i in range(1, 5):
+        cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                       (f'newuser {i}', f'newuser{i}@mail.ru', f'{i * 10 + 10}', 1000))
+
+    connection.commit()
+    connection.close()
+
+
+def add_user(username, email, age):
+    connection = sqlite3.connect('users.db')
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                   (username, email, age, 1000))
+    connection.commit()
+    connection.close()
+
+
+def is_included(username):
+    connection = sqlite3.connect('users.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM Users WHERE  username=?', (username,))
+    check_users = cursor.fetchall()
+    if not check_users:
+        connection.close()
+        return False
+    else:
+        connection.close()
+        return True
 
 
 def get_all_products():
